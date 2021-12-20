8639f3cb6a804c68a14ee1736190b2809cd8ee75
rbueso <rbueso@telynet.com>
2021-12-20 18:46:52 +0100

    añadido sensores

--- autonomo.ts
+++ autonomo.ts
@@ -2,6 +2,7 @@ export interface Autonomo {
     nivelAutomatizacion: number;
     precisionGPS: number;
     camaras: number;
+    sensores: number;
     machineLearning: boolean;
     navegacionInercial: boolean;
 }
